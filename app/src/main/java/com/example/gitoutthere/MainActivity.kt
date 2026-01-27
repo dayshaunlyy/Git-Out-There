04a6ce32a3e1b41f0971aecf6b09b8a157d63627
T_Kerr <tkerr@csumb.edu>
2026-01-27 15:33:50 -0800

    Test Commit
    changed the name from "android" to "Group 4"

--- app/src/main/java/com/example/gitoutthere/MainActivity.kt
+++ app/src/main/java/com/example/gitoutthere/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GitOutThereTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Group 4",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello, $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GitOutThereTheme {
-        Greeting("Android")
+        Greeting("Group 4")
     }
 }
