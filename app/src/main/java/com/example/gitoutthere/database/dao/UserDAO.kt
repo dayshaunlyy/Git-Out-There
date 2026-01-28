7f2f6b9963eb716ebdec8e843556b738729e4068
natetowsley <natetowsley@gmail.com>
2026-01-27 18:59:13 -0800

    User Repository created, small adjustments to User Dao.

--- app/src/main/java/com/example/gitoutthere/database/dao/UserDAO.kt
+++ app/src/main/java/com/example/gitoutthere/database/dao/UserDAO.kt
@@ -6,10 +6,13 @@ import com.example.gitoutthere.database.entities.User
 @Dao
 interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.ABORT)
-    fun insertUser(vararg user: User): Int
+    suspend fun insertUser(user: User): Int
 
     @Query("SELECT * FROM users WHERE username = :username") // Check for users w/ desired username
-    fun getUser(username: String): User?
+    suspend fun getUserByUsername(username: String): User?
+
+    @Query("SELECT EXISTS(SELECT 1 FROM users WHERE username = :username)")
+    suspend fun isUsernameExists(username: String): Boolean
 
     //TODO: @Query for getting user(s)
 }
