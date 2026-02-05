6a0eeab72e8806503c92b83ba20f3a2577818b24
Nate Towsley <natetowsley@gmail.com>
2026-02-05 08:39:59 -0800

    Get readme api route created, readme object created.

--- app/src/main/java/com/example/gitoutthere/api/GitHubRepository.kt
+++ app/src/main/java/com/example/gitoutthere/api/GitHubRepository.kt
@@ -4,4 +4,6 @@ package com.example.gitoutthere.api
 class GitHubRepository (private val api : GitHubApi = RetrofitClient.api){
     suspend fun searchRepos(q: String) = api.searchRepos(q)
 
+    suspend fun getReadme(owner: String, repo: String) = api.getReadme(owner, repo)
+
 }
