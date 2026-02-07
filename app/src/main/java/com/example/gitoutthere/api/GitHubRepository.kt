c13398b57c674786834848f5a9bf04b7e0eeaa03
natetowsley <natetowsley@gmail.com>
2026-02-06 18:55:29 -0800

    Now storing issue url, labels, and issue number. Label DTO created. getIssues() adjusted temporarily to look into response accuracy.

--- app/src/main/java/com/example/gitoutthere/api/GitHubRepository.kt
+++ app/src/main/java/com/example/gitoutthere/api/GitHubRepository.kt
@@ -1,11 +1,17 @@
 package com.example.gitoutthere.api
 
-
 class GitHubRepository (private val api : GitHubApi = RetrofitClient.api){
     suspend fun searchRepos(q: String) = api.searchRepos(q)
 
     suspend fun getReadme(owner: String, repo: String) = api.getReadme(owner, repo)
 
-    suspend fun getIssues(owner: String, repo: String) = api.getIssues(owner, repo)
+    suspend fun getIssues(owner: String, repo: String): List<IssueDto> {
+        val issues = api.getIssues(owner, repo)
+        println("GitHub Issues Response: Received ${issues.size} issues.")
+        issues.forEach { issue ->
+            println(issue)
+        }
+        return issues
+    }
 
 }
--- app/src/main/java/com/example/gitoutthere/api/IssueDto.kt
+++ app/src/main/java/com/example/gitoutthere/api/IssueDto.kt
@@ -1,7 +1,13 @@
 package com.example.gitoutthere.api
 
+import com.google.gson.annotations.SerializedName
+
 data class IssueDto(
     val id: Long,
+    val number: Int,
+    @SerializedName("html_url")
+    val htmlUrl: String,
     val title: String,
-    val body: String
+    val body: String,
+    val labels: List<LabelDto>
 )
